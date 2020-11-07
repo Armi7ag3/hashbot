8e7f2c703a53b87664c60584e658c75b36f9df5c
Armi7ag3 <texnl7@gmail.com>
2020-11-07 23:52:38 +0300

    commit

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def handle_docs_photo(message):
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
 
-        src = 'E:/1/' + message.document.file_name 
+        src = 'C:/Users/user/Desktop/1/' + message.document.file_name 
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
 
