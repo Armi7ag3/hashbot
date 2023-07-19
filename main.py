1fccbab30d583ebe39afc2fdd3c77a62b9da7c0e
Armi7ag3 <58391302+Armi7ag3@users.noreply.github.com>
2023-07-19 13:59:40 +0000

    linter + old code remove

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import telebot
+"""Module providingFunction of telegram hash bot"""
 import os
 import subprocess
+import telebot
 from telebot import types
-capfile = False
-directory = 'E:/1'
-target = '1.hccapx'
+# capfile = False
+DIRECTORY = 'E:/1'
+TARGET = '1.hccapx'
 bot = telebot.TeleBot('1063868677:AAGfaq8tqOG4Z_kAiAhS3WNviczNBJG02dY')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    """Module providingFunction of tbot messaging"""
     if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+        bot.send_message(message.from_user.id, "Привет,сегодня.")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши Привет")
     else:
         bot.send_message(message.from_user.id, "Цель установлена")
-        ttt = open('target.txt','w')
-        ttt.write(directory + '/' + message.text)
+        ttt = open('target.txt','w', encoding="utf-8")
+        ttt.write(DIRECTORY + '/' + message.text)
         ttt.close()
     # Готовим кнопки
     keyboard = types.InlineKeyboardMarkup()
@@ -31,22 +33,24 @@ def get_text_messages(message):
 # Обработчик нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-  # Если нажали на одну из 12 кнопок — выводим гороскоп
-    if call.data == "capfile": 
+    """Module providingFunction of tbot button press"""
+    if call.data == "capfile":
         bot.send_message(call.message.chat.id, 'ожидаю файл')
-        capfile = True
+        # capfile = True
     if call.data == "filelist":
         msg = ''
-        files = os.listdir(directory)
+        files = os.listdir(DIRECTORY)
         for i in files:
             msg = msg + '\n' + i
         bot.send_message(call.message.chat.id, msg)
     if call.data == "filecrack":
-        ttt = open('target.txt','r')
+        ttt = open('target.txt','r', encoding="utf-8")
         target = ttt.readline()
-        ttt.close
-        args = ['C:/Users/User/Desktop/hashcat-6.1.1/hashcat.exe', '-a', '3', '-m', '2500', '--quiet', target, '?d?d?d?d?d?d?d?d']
-        child = subprocess.Popen(args, cwd='C:/Users/User/Desktop/hashcat-6.1.1', stdout=subprocess.PIPE, encoding='utf8')
+        ttt.close()
+        args = ['C:/Users/User/Desktop/hashcat-6.1.1/hashcat.exe', '-a', '3',
+                '-m', '2500', '--quiet', target, '?d?d?d?d?d?d?d?d']
+        child = subprocess.Popen(args, cwd='C:/Users/User/Desktop/hashcat-6.1.1',
+                                 stdout=subprocess.PIPE, encoding='utf8')
         data = child.communicate()
             #for line in data:
             #bot.send_message(call.message.chat.id, line)
@@ -54,20 +58,21 @@ def callback_worker(call):
         while child.poll() is None:
             for line in data:
                 print (line)
-        
+
 @bot.message_handler(content_types=['document'])
 def handle_docs_photo(message):
+    """Module providingFunction of tbot file load"""
     try:
         #chat_id = message.chat.id
 
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
 
-        src = 'E:/1/' + message.document.file_name 
+        src = 'E:/1/' + message.document.file_name
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
 
         bot.reply_to(message, "Пожалуй, я сохраню это")
-    except Exception as e:
-        bot.reply_to(message, e)
+    except OSError as os_error:
+        bot.reply_to(message, os_error)
 bot.polling(none_stop=True, interval=0)
